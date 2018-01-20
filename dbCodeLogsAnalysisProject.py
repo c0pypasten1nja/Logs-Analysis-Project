833b6f5495b658532d53aa83d16c1c038e801f15
ian <yeowian@gmail.com>
2018-01-21 00:05:18 +0800

    checks

--- dbCodeLogsAnalysisProject.py
+++ dbCodeLogsAnalysisProject.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-# Database code for Logs Analysis Project
 
 import psycopg2
 
@@ -8,47 +7,47 @@ db = psycopg2.connect("dbname=news")
 c = db.cursor()
 
 qn1 = "What are the most popular three articles of all time?"
-query1 = """select articles.title, count(*) as views
-        from articles join log
-            on concat('/article/', articles.slug) = log.path
-        where log.status = '200 OK'
-        group by articles.title
-        order by views desc limit 3;"""
+query1 = """SELECT articles.title, count(*) AS views
+        FROM articles JOIN log
+            ON concat('/article/', articles.slug) = log.path
+        WHERE log.status = '200 OK'
+        GROUP BY articles.title
+        ORDER BY views DESC LIMIT 3;"""
 
 print(qn1)
 c.execute(query1)
 results = c.fetchall()
 for title, views in results:
-    print "{} -- {} views".format(title, views)
-print ""
+    print("{} -- {} views").format(title, views)
+print("")
 
 qn2 = "Who are the most popular article authors of all time?"
-query2 = """select authors.name, count(*) as views
-            from authors join articles
-                on authors.id = articles.author
-                join log on concat('/article/', articles.slug) = log.path
-            where log.status = '200 OK'
-            group by authors.name
-            order by views desc;"""
+query2 = """SELECT authors.name, count(*) AS views
+            FROM authors JOIN articles
+                ON authors.id = articles.author
+                JOIN log ON concat('/article/', articles.slug) = log.path
+            WHERE log.status = '200 OK'
+            GROUP BY authors.name
+            ORDER BY views DESC;"""
 
 print(qn2)
 c.execute(query2)
 results = c.fetchall()
 for title, views in results:
-    print "{} -- {} views".format(title, views)
-print ""
+    print("{} -- {} views").format(title, views)
+print("")
 
 qn3 = "On which days did more than 1% of requests lead to errors?"
-query3 = """select day, erp from (select date(time) as day,
-            round(((count(status) filter (where log.status != '200 OK')*100.00)
-            /count(method)),2) as erp from log group by 1)
-            as op where erp > 1.0;"""
+query3 = """SELECT day, erp FROM (SELECT date(time) AS day,
+            round(((count(status) filter (WHERE log.status != '200 OK')*100.00)
+            /count(method)),2) AS erp FROM log GROUP BY 1)
+            AS op WHERE erp > 1.0;"""
 
 print(qn3)
 c.execute(query3)
 results = c.fetchall()
 for title, erp in results:
-    print "{} -- {} % errors".format(title, erp)
-print ""
+    print("{} -- {} % errors").format(title, erp)
+print("")
 
 db.close()
