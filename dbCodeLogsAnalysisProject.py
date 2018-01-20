87736e0bc9995ac324ebad25e08930ac8927eb86
ian <yeowian@gmail.com>
2018-01-20 15:26:06 +0800

    1. include a shebang line as the very first line of Python code file 2. remove bleach library 3. recode print results 4. change one of the values in the calculation to a type other than integer

--- dbCodeLogsAnalysisProject.py
+++ dbCodeLogsAnalysisProject.py
@@ -1,7 +1,7 @@
+#!/usr/bin/env python
 # Database code for Logs Analysis Project
 
 import psycopg2
-import bleach
 
 # connect to the database
 db = psycopg2.connect("dbname=news")
@@ -18,9 +18,8 @@ query1 = """select articles.title, count(*) as views
 print(qn1)
 c.execute(query1)
 results = c.fetchall()
-for i in range(len(results)):
-    print results[i][0], '--', results[i][1], ' views'
-    i = i + 1
+for title, views in results:
+    print "{} -- {} views".format(title, views)
 print ""
 
 qn2 = "Who are the most popular article authors of all time?"
@@ -30,28 +29,26 @@ query2 = """select authors.name, count(*) as views
                 join log on concat('/article/', articles.slug) = log.path
             where log.status = '200 OK'
             group by authors.name
-            order by views desc limit 3;"""
+            order by views desc;"""
 
 print(qn2)
 c.execute(query2)
 results = c.fetchall()
-for i in range(len(results)):
-    print results[i][0], '--', results[i][1], ' views'
-    i = i + 1
+for title, views in results:
+    print "{} -- {} views".format(title, views)
 print ""
 
 qn3 = "On which days did more than 1% of requests lead to errors?"
 query3 = """select day, erp from (select date(time) as day,
-            round(((count(status) filter (where log.status != '200 OK')*100)
+            round(((count(status) filter (where log.status != '200 OK')*100.00)
             /count(method)),2) as erp from log group by 1)
             as op where erp > 1.0;"""
 
 print(qn3)
 c.execute(query3)
 results = c.fetchall()
-for i in range(len(results)):
-    print results[i][0], '--', results[i][1], '% errors'
-    i = i + 1
+for title, erp in results:
+    print "{} -- {} % errors".format(title, erp)
 print ""
 
 db.close()
